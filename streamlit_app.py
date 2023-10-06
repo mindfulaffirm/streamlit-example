1d303a5333cc438c588890452a79c8362b67cd0d
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 14:23:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,29 @@
-# Importing necessary libraries
+# Import necessary libraries
 import streamlit as st
 
-# Title for the app
-st.title("Square Calculator")
+def main():
+    # Title of the app
+    st.title("Chat Simulation")
 
-# Taking a number input from the user
-number = st.number_input("Enter a number", value=1.0)
+    # Store the conversation history in a session state if it doesn't exist
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
 
-# Calculating the square and displaying it
-st.write(f"The square of {number} is {number**2}")
+    # Text box for user input
+    user_input = st.text_input("You:")
+
+    # Submit button to send the message
+    if st.button("Submit"):
+        # Append the user's message to the conversation history
+        st.session_state.conversation_history.append(f"You: {user_input}")
+        
+        # Here you can add some logic for generating a response, for now, let's just echo the user's message
+        response = f"ChatGPT: I received your message: '{user_input}'."
+        st.session_state.conversation_history.append(response)
+
+    # Display the conversation history
+    for message in st.session_state.conversation_history:
+        st.write(message)
+
+if __name__ == "__main__":
+    main()
