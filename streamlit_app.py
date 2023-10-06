c3d6c7e6efee1462f513dc4a8c1479028f4a063e
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 14:42:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,9 @@
+# Import necessary libraries
 import streamlit as st
-import openai
-
-# Set up OpenAI GPT-3 API
-openai.api_key = 'YOUR_OPENAI_API_KEY'
-
-def get_gpt3_response(prompt):
-    """Function to get GPT-3's response to a given prompt."""
-    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=150)
-    return response.choices[0].text.strip()
 
 def main():
-    st.title("Chat Simulation with GPT-3")
+    # Title of the app
+    st.title("Chat Simulation")
 
     # Store the conversation history in a session state if it doesn't exist
     if 'conversation_history' not in st.session_state:
@@ -23,10 +16,10 @@ def main():
     if st.button("Submit"):
         # Append the user's message to the conversation history
         st.session_state.conversation_history.append(f"You: {user_input}")
-
-        # Get GPT-3's response
-        gpt3_response = get_gpt3_response(user_input)
-        st.session_state.conversation_history.append(f"ChatGPT: {gpt3_response}")
+        
+        # Here you can add some logic for generating a response, for now, let's just echo the user's message
+        response = f"ChatGPT: I received your message: '{user_input}'."
+        st.session_state.conversation_history.append(response)
 
     # Display the conversation history
     for message in st.session_state.conversation_history:
