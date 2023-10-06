c5c7c3770e4170c396193f1f64095660762c9339
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 15:04:56 +0800

    Update and rename oldFiles to oldFiles.py

---
+++ oldFiles.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import openai
+
+# Set up OpenAI GPT-3 API
+openai.api_key = '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'
+
+def get_gpt3_response(prompt):
+    """Function to get GPT-3's response to a given prompt."""
+    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=150)
+    return response.choices[0].text.strip()
+
+def main():
+    st.title("Chat Simulation with GPT-3")
+
+    # Store the conversation history in a session state if it doesn't exist
+    if 'conversation_history' not in st.session_state:
+        st.session_state.conversation_history = []
+
+    # Text box for user input
+    user_input = st.text_input("You:")
+
+    # Submit button to send the message
+    if st.button("Submit"):
+        # Append the user's message to the conversation history
+        st.session_state.conversation_history.append(f"You: {user_input}")
+        
+        # Get GPT-3's response
+        gpt3_response = get_gpt3_response(user_input)
+        st.session_state.conversation_history.append(f"ChatGPT: {gpt3_response}")
+
+    # Display the conversation history
+    for message in st.session_state.conversation_history:
+        st.write(message)
+
+if __name__ == "__main__":
+    main()
+
