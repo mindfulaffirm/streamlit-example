fc38f42feebbf4e7e5f80b499a947b5935ba37ab
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 12:49:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,45 @@
-# Importing necessary libraries
 import streamlit as st
+import numpy as np
 
-# Title for the app
-st.title("Square Calculator")
+# Function to calculate entropy of the sequence
+def calculate_entropy(sequence):
+    # If sequence is empty, return 0
+    if len(sequence) == 0:
+        return 0
 
-# Taking a number input from the user
-number = st.number_input("Enter a number", value=1.0)
+    # Count frequency of each box click
+    values, counts = np.unique(sequence, return_counts=True)
+    probs = counts / len(sequence)
+    entropy = -np.sum(probs * np.log2(probs))
+    return entropy
 
-# Calculating the square and displaying it
-st.write(f"The square of {number} is {number**2}")
+# App title
+st.title("3x3 Box Entropy Calculator")
+
+# State management for Streamlit (to remember the sequence across reruns)
+if 'sequence' not in st.session_state:
+    st.session_state.sequence = []
+
+# Display 3x3 grid and handle box clicks
+for i in range(3):
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        if st.button(f"Box {3*i + 1}"):
+            st.session_state.sequence.append(3*i + 1)
+    with col2:
+        if st.button(f"Box {3*i + 2}"):
+            st.session_state.sequence.append(3*i + 2)
+    with col3:
+        if st.button(f"Box {3*i + 3}"):
+            st.session_state.sequence.append(3*i + 3)
+
+# Display recorded sequence
+st.write(f"Sequence: {st.session_state.sequence}")
+
+# Calculate and display entropy
+entropy = calculate_entropy(st.session_state.sequence)
+st.write(f"Entropy: {entropy}")
+
+# A button to reset the sequence if needed
+if st.button("Reset Sequence"):
+    st.session_state.sequence = []
