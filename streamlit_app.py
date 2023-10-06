916efd3fa14f732ee56e9de4ca1f4d8ab4a0b49b
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 15:09:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,29 @@
 import streamlit as st
-import openai
+import requests
 
-# Set up OpenAI GPT-3 API
-openai.api_key = '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'
+# Set up Azure OpenAI API endpoint and key
+AZURE_ENDPOINT = 'https://nie-gpt.openai.azure.com/openai/deployments/GPT_35_TURBO/chat/completions?api-version=2023-06-01-preview&api-key=250783a2a3ed4cbe93dd0d7d2c443144'  # Replace with your endpoint
+AZURE_API_KEY = '250783a2a3ed4cbe93dd0d7d2c443144'  # Replace with your key
 
-def get_gpt3_response(prompt):
-    """Function to get GPT-3's response to a given prompt."""
-    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=150)
-    return response.choices[0].text.strip()
+headers = {
+    "Authorization": f"Bearer {AZURE_API_KEY}",
+    "Content-Type": "application/json"
+}
+
+def get_gpt3_response_from_azure(prompt):
+    """Function to get GPT-3's response from Azure's OpenAI service."""
+    data = {
+        "prompt": prompt,
+        "max_tokens": 150
+    }
+    response = requests.post(AZURE_ENDPOINT, headers=headers, json=data)
+    response_json = response.json()
+
+    # Extract and return the response text from the JSON response (adjust as per Azure's response structure)
+    return response_json['choices'][0]['text'].strip()
 
 def main():
-    st.title("Chat Simulation with GPT-3")
+    st.title("Chat Simulation with Azure OpenAI GPT-3")
 
     # Store the conversation history in a session state if it doesn't exist
     if 'conversation_history' not in st.session_state:
@@ -24,8 +37,8 @@ def main():
         # Append the user's message to the conversation history
         st.session_state.conversation_history.append(f"You: {user_input}")
         
-        # Get GPT-3's response
-        gpt3_response = get_gpt3_response(user_input)
+        # Get GPT-3's response from Azure
+        gpt3_response = get_gpt3_response_from_azure(user_input)
         st.session_state.conversation_history.append(f"ChatGPT: {gpt3_response}")
 
     # Display the conversation history
