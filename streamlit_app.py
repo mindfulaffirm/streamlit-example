aee71d236588a36774c88b711b86740bd13dd0cb
mindfulaffirm <63384888+mindfulaffirm@users.noreply.github.com>
2023-10-06 14:13:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,11 @@
+# Importing necessary libraries
 import streamlit as st
 
-# Initializing session state variables if they don't exist
-if 'button1_color' not in st.session_state:
-    st.session_state.button1_color = 'primary'
-if 'button2_color' not in st.session_state:
-    st.session_state.button2_color = 'primary'
+# Title for the app
+st.title("Square Calculator")
 
-# If Button 1 is clicked, toggle its color
-if st.button('Button 1', key='btn1', on_click=lambda: setattr(st.session_state, 'button1_color', 'danger' if st.session_state.button1_color == 'primary' else 'primary'), color=st.session_state.button1_color):
-    st.write('Button 1 clicked!')
+# Taking a number input from the user
+number = st.number_input("Enter a number", value=1.0)
 
-# If Button 2 is clicked, toggle its color
-if st.button('Button 2', key='btn2', on_click=lambda: setattr(st.session_state, 'button2_color', 'danger' if st.session_state.button2_color == 'primary' else 'primary'), color=st.session_state.button2_color):
-    st.write('Button 2 clicked!')
+# Calculating the square and displaying it
+st.write(f"The square of {number} is {number**2}")
